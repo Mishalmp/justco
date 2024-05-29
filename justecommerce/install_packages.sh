1dda98501601cc42b8cee2d34ddc5c20740c0935
Mishal Mp <mishalmp916@gmail.com>
2024-05-29 16:45:43 +0530

    bash added

--- justecommerce/install_packages.sh
+++ justecommerce/install_packages.sh
@@ -2,7 +2,7 @@
 
 
 # Update package lists
-apt-get update --no-install-recommends
+sudo apt-get update --no-install-recommends
 
 # Install python3-tk
-apt-get install -y --no-install-recommends python3-tk
+sudo apt-get install -y --no-install-recommends python3-tk
